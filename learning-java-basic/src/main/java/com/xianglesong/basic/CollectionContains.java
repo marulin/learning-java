6579ba61b89b64053e50da81942491d047af561b
marulin <rollin.r.ma@gmail.com>
2017-02-08 14:42:27 +0800

    add collection contains example.

--- learning-java-basic/src/main/java/com/xianglesong/basic/CollectionContains.java
+++ learning-java-basic/src/main/java/com/xianglesong/basic/CollectionContains.java
@@ -3,7 +3,7 @@ package com.xianglesong.basic;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListContains {
+public class CollectionContains {
 
   public static void main(String[] args) {
     List<Person> list = new ArrayList<Person>();
