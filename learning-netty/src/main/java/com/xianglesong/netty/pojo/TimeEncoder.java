c3c85b33584b5795b238c575d62bdbc94975fb47
marulin <rollin.r.ma@gmail.com>
2017-02-20 17:03:53 +0800

    add

---
+++ learning-netty/src/main/java/com/xianglesong/netty/pojo/TimeEncoder.java
@@ -0,0 +1,14 @@
+package com.xianglesong.netty.pojo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class TimeEncoder extends MessageToByteEncoder<UnixTime> {
+  @Override
+  protected void encode(ChannelHandlerContext ctx, UnixTime msg, ByteBuf out) {
+    out.writeInt((int) msg.value());
+  }
+}
